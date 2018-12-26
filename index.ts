836d12fbfaa22a6870098368d2e08f59f8fc8cd3
Anne A <aaguirre.rdit@gmail.com>
2018-12-26 16:33:25 +0900

    added normalizePort function

--- index.ts
+++ index.ts
@@ -4,20 +4,20 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import http from 'http';
 import logger from 'morgan';
-import { authMiddleware } from "./src/middlewares";
+import { authMiddleware } from './src/middlewares';
+import { normalizePort } from "./src/util";
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(authMiddleware);
-const port = 5000;
+const port = normalizePort(process.env.PORT || 5000);
 app.set('port', port);
 app.use(cors());
 app.use(router);
 const server = http.createServer(app);
 
-server.listen(port,() => {
-    console.log('server on 5000');
+server.listen(port, () => {
+  console.log('server on 5000');
 });
-
